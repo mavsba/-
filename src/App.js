ed65d97cd19271b0b5f66de0af8223c2950e7283
mavsba <44697102+mavsba@users.noreply.github.com>
2019-06-20 09:50:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { GlobalStyle } from './style'
+import { Provider } from 'react-redux'
+import { BrowserRouter,Switch,Route } from 'react-router-dom'
+import store from './store'
 
 import Header from './common/header'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+import Login from './pages/Login/index'
+
+
+
+
 
 function App() {
   return (
-    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+        <Header></Header>
+        <Switch> {/*只匹配其中一个*/}
+          <Route path='/detail:id' component={Detail}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
       <GlobalStyle></GlobalStyle>
-     <Header></Header>    
-    </div>  
+    </Provider> 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './statics/iconfont/iconfont.css'
 // import './nb.css'
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
